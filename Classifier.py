17081a77a36ef116f787cb0646283c6416bc34c2
lily-bjork <59908201+lily-bjork@users.noreply.github.com>
2023-03-10 19:35:55 -0800

    Saved model

--- Classifier.py
+++ Classifier.py
@@ -1,3 +1,6 @@
+from sklearn import model_selection, datasets
+import joblib
+import pickle
 from RNN import classifyNameList, getModel, classify, getTestData
 from AlgoTester import testAlgorithm
 
@@ -11,12 +14,20 @@ def createOutputFile(filename, classifications, names):
 		outfile.write(f"{name}: {classifications[i]}\n")
 		i+=1
 
+#train and save a new model
+def saveModel():
+	filename = "Model.joblib"
+	joblib.dump(getModel(),filename)
+
+#fetch the model
 def setupAlgorithm():
-	# Train the RNN model
-	RNN = getModel()
+	filename = "Model.joblib"
+	loaded_model = joblib.load(filename)
 
-	return RNN
+	return loaded_model
 
+# Uncomment to train a new model
+# saveModel()
 RNN = setupAlgorithm()
 
 def classifyNames(filename, RNN = RNN):
@@ -25,15 +36,15 @@ def classifyNames(filename, RNN = RNN):
 	
 	createOutputFile(filename, mylst, names)
 
-# Needs to print to a file, not to terminal
-classifiedNames = classifyNames("test_files/akkadian_test.txt")
+# # Needs to print to a file, not to terminal
+# classifiedNames = classifyNames("test_files/akkadian_test.txt")
 
 
-# Write a function that waits for user in
 
 # Tests the Accuracy and precision before and after ripper algorithm
+
 testData = getTestData()
 classifiedNames = classifyNameList(testData, RNN)
 testAlgorithm(classifiedNames, testData)
 
-
+# classifyNames("SumerianDBNames.txt")
